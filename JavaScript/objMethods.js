1a72c90c8995037ba4d71e233916007c6a5b9857
dsr <deepak@gsat.us>
2022-07-20 14:30:30 +0530

    fist commit

---
+++ JavaScript/objMethods.js
@@ -0,0 +1,11 @@
+var details = {
+    name : "Cherry",
+    age : 22,
+    print : function(){
+        console.log("hello")
+        return 10
+    },
+}
+
+console.log(details.name)
+console.log(details.print())
